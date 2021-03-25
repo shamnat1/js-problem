38518461fc74c8fbaa9e366014447c283da148e4
Benhur D <benhur.davies@softgames.de>
2021-03-25 16:28:31 +0100

    adds anagram-palindrome

---
+++ src/problem-solving/anagram-palindrome.test.js
@@ -0,0 +1,31 @@
+/**
+ * checking to see if the given string can be rearranged into a palindrome.
+ *
+ * Example
+ * s = 'aabbccdd'
+ * One way this can be arranged into a palindrome is abcddcba. Return true.
+ *
+ * Constraints
+ * contains only lowercase letters in the range ascii[a..z]
+ */
+
+const fs = require("fs");
+const path = require("path");
+const isPalindromePossible = require("./anagram-palindrome.js");
+
+describe("anagram-palindrome", () => {
+  it("should return true for possible to make palindrome", () => {
+    expect(isPalindromePossible("aaabbbb")).toBe(true);
+    expect(isPalindromePossible("cdcdcdcdeeeef")).toBe(true);
+
+    const bigContent = fs.readFileSync(path.resolve(__dirname, "../test-data/anagram-palindrome-success1.txt"),"ascii");
+    expect(isPalindromePossible(bigContent)).toBe(true);
+  });
+
+  it("should return false for if not possible to make palindrome", () => {
+    expect(isPalindromePossible("cdefghmnopqrstuvw")).toBe(false);
+
+    const bigContent = fs.readFileSync(path.resolve(__dirname, "../test-data/anagram-palindrome-fail1.txt"),"ascii");
+    expect(isPalindromePossible(bigContent)).toBe(false);
+  });
+});
