88ed1721f61093330be4b835dccd023aee9cae54
Rene Simon <mail@rene-simon.eu>
2021-03-31 14:59:46 +0200

    Adds concurrency test

---
+++ src/problem-solving/07_concurrency.js
@@ -0,0 +1,9 @@
+/**
+ * Export a function named 'count'.
+ * It should call the given callback every 10th of a second with a increment from given start to end integer
+ *
+ * @param {number} start - Given to callback first
+ * @param {number} end - Stop when count reached this value
+ * @param {function(number): void} callback - Called after each 100ms with an increment
+ * @returns {function(): void}
+ */
